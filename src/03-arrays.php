be32754bd448d3f5de0b7da62b270d8200a773ac
Illarion Ivanchenko <larik.maths@gmail.com>
2021-01-22 13:08:05 +0200

    feat: 03-arrays is implemented

--- src/03-arrays.php
+++ src/03-arrays.php
@@ -5,11 +5,24 @@
  * without changing the order.
  * Example: [1,3,2] => [1,3,3,3,2,2]
  *
- * @param  array  $input
+ * @param array $input
  * @return array
  */
 function repeatArrayValues(array $input)
 {
+    // Formation of the result
+    $result = [];
+
+    // For each element, duplicate it the required number of times
+    foreach ($input as $item) {
+        // Check that the number is integer and positive, then duplicate
+        if (is_int($item) && $item > 0) {
+            $result = array_pad($result, count($result) + $item, $item);
+        }
+    }
+
+    // Return the result
+    return $result;
 }
 
 /**
@@ -17,11 +30,32 @@ function repeatArrayValues(array $input)
  * Return the lowest unique value or 0 if there is no unique values or array is empty.
  * Example: [1, 2, 3, 2, 1, 5, 6] => 3
  *
- * @param  array  $input
+ * @param array $input
  * @return int
  */
 function getUniqueValue(array $input)
 {
+    // If the array is empty then return 0
+    if (empty($input)) return 0;
+
+    // Formation of the result
+    $result = null;
+    $key_last = array_key_last($input);
+
+    // Compare each element with others
+    foreach ($input as $key1 => $value1) {
+        foreach ($input as $key2 => $value2) {
+            // If the element is not unique, go to the next
+            if ($input[$key1] === $input[$key2] && $key1 != $key2) break 1;
+            // If the element is the smallest of the unique ones, then save it
+            if ($key2 === $key_last && (is_null($result) || $result > $input[$key1])) {
+                $result = $input[$key1];
+            }
+        }
+    }
+
+    // If the result is empty return 0, else return the result
+    return is_null($result) ? 0 : $result;
 }
 
 /**
@@ -45,9 +79,32 @@ function getUniqueValue(array $input)
  *  'yellow' => ['orange', 'potato'],
  * ]
  *
- * @param  array  $input
+ * @param array $input
  * @return array
  */
 function groupByTag(array $input)
 {
+    // Formation of the result
+    $result = [];
+
+    // The value with the key 'tags'
+    // becomes the keys of the resulting array
+    foreach ($input as $key => $item) {
+        foreach ($item['tags'] as $tag) {
+            // Add a new name element to the result
+            if (array_key_exists($tag, $result)) {
+                array_push($result[$tag], $item['name']);
+            } else {
+                $result[$tag] = [$item['name']];
+            }
+        }
+    }
+
+    // Sorting the result
+    foreach ($result as $key => $subarr) {
+        sort($result[$key]);
+    }
+
+    // Return the result
+    return $result;
 }
