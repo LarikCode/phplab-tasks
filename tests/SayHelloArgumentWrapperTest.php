99fe834d738a5fc2fc4351dd6f7a4ba77c78f6ce
Illarion Ivanchenko <larik.maths@gmail.com>
2021-01-24 14:17:34 +0200

    feat: SayHelloArgumentWrapperTest implemented

---
+++ tests/SayHelloArgumentWrapperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SayHelloArgumentWrapperTest extends TestCase
+{
+    /**
+     * @dataProvider negativeDataProvider
+     */
+    public function testNegative($input)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        sayHelloArgumentWrapper($input);
+    }
+
+    public function negativeDataProvider()
+    {
+        return [
+            [[]],
+            [['Tom']],
+            [['John']],
+            [[['Hello'],['world']]],
+            [function(){return 0;}]
+        ];
+    }
+}
