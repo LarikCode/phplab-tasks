9a28dd2735fb7e40e26860eb9c19817dc1e9ed1e
Illarion Ivanchenko <larik.maths@gmail.com>
2021-02-04 21:51:50 +0200

    refactor: added new examples

--- src/oop/example.php
+++ src/oop/example.php
@@ -1,11 +1,17 @@
 <?php
 use src\oop\Calculator;
+use src\oop\Commands\DivCommand;
+use src\oop\Commands\ExpCommand;
+use src\oop\Commands\MulCommand;
 use src\oop\Commands\SubCommand;
 use src\oop\Commands\SumCommand;
 
 $calc = new Calculator();
 $calc->addCommand('+', new SumCommand());
 $calc->addCommand('-', new SubCommand());
+$calc->addCommand('*', new MulCommand());
+$calc->addCommand('/', new DivCommand());
+$calc->addCommand('**', new ExpCommand());
 
 // You can use any operation for computing
 // will output 2
@@ -41,6 +47,30 @@ echo $calc->init(1)
     ->compute('+', 5)
     ->undo()
     ->undo()
+    ->getResult();
+
+echo PHP_EOL;
+
+// should output 30
+echo $calc->init(2)
+    ->compute('*', 3)
+    ->compute('*', 5)
+    ->getResult();
+
+echo PHP_EOL;
+
+// should output 5
+echo $calc->init(30)
+    ->compute('/', 2)
+    ->compute('/', 3)
+    ->getResult();
+
+echo PHP_EOL;
+
+// should output 64
+echo $calc->init(2)
+    ->compute('**', 2)
+    ->compute('**', 3)
     ->getResult();
 
 echo PHP_EOL;
