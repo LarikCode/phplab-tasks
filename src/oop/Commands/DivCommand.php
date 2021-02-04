71102cc43e7d637e3eb92e14c7041debe866bfaf
Illarion Ivanchenko <larik.maths@gmail.com>
2021-02-04 15:28:26 +0200

    feat: class DivCommand implemented

---
+++ src/oop/Commands/DivCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace src\oop\Commands;
+
+
+class DivCommand implements CommandInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function execute(...$args)
+    {
+        if (2 != sizeof($args)) {
+            throw new \InvalidArgumentException('Not enough parameters');
+        }
+
+        if ($args[1] == 0) {
+            throw new \InvalidArgumentException('You cannot divide by zero');
+        }
+
+        return $args[0] / $args[1];
+    }
+}
